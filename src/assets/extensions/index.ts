76304a32e6665593f61327e1e611ac9b202589df
Embers-of-the-Fire <stellarishs@163.com>
2026-03-23 00:26:28 +0800

    rm: remove unused assets

--- src/assets/extensions/index.ts
+++
@@ -1,5 +0,0 @@
-import bookmarkIconUrl from "./icons/bookmark.svg?url";
-import dataSiteIconUrl from "./icons/data-site.svg?url";
-import relicLaneImageUrl from "./images/relic-lane.svg?url";
-
-export { bookmarkIconUrl, dataSiteIconUrl, relicLaneImageUrl };
