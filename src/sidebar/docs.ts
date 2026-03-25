1b28270c31e0cfdc51bc3963aed434e7f422448c
Embers-of-the-Fire <stellarishs@163.com>
2026-03-26 07:41:20 +0800

    docs: add thirdparty links

--- src/sidebar/docs.ts
+++ src/sidebar/docs.ts
@@ -117,4 +117,54 @@ export const docsSidebar: Sidebar = [
         collapsed: true,
         autogenerate: { directory: "guides/expert" },
     },
+    {
+        label: "第三方网站",
+        collapsed: true,
+        items: [
+            {
+                label: "EVE University",
+                link: "https://wiki.eveuniversity.org/Main_Page",
+            },
+            {
+                label: "CEVE Market",
+                link: "https://www.ceve-market.org/index/"
+            },
+            {
+                label: "Anoikis",
+                link: "https://anoik.is/"
+            },
+            {
+                label: "爱神虫洞",
+                link: "http://venus5.wormholes.club/search.php?lang=zh"
+            },
+            {
+                label: "EVE Scout",
+                link: "https://eve-scout.com/#/"
+            },
+            {
+                label: "DotLAN",
+                link: "https://evemaps.dotlan.net/"
+            },
+            {
+                label: "EVE Gatecamp Check",
+                link: "https://eve-gatecheck.space/eve/",
+            },
+            {
+                label: "PyFA",
+                link: "https://github.com/pyfa-org/Pyfa",
+            },
+            {
+                label: "ZKillboard",
+                link: "https://zkillboard.com/",
+            },
+            {
+                label: "CEVE Killboard",
+                link: "https://kb.ceve-market.org/",
+            },
+            {
+                label: "Daily Sov Maps",
+                link: "https://www.verite.space/",
+            }
+        ]
+    }
 ];
