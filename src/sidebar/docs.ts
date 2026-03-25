6a76cd8d4b1d567f122d3857851951652b9968c1
Embers-of-the-Fire <stellarishs@163.com>
2026-03-25 09:13:50 +0800

    docs: add basic exploration docs

--- src/sidebar/docs.ts
+++ src/sidebar/docs.ts
@@ -14,4 +14,38 @@ export const docsSidebar: Sidebar = [
         label: "快速指南",
         link: "/guides/quick-guide",
     },
+    {
+        label: "入门探索",
+        items: [
+            {
+                label: "扫描操作",
+                link: "/guides/basic-exploration/probing",
+            },
+            {
+                label: "信号类型",
+                link: "/guides/basic-exploration/site-types",
+            },
+            {
+                label: "扫描地区",
+                items: [
+                    {
+                        label: "总览",
+                        link: "/guides/basic-exploration/areas",
+                    },
+                    {
+                        label: "虫洞",
+                        link: "/guides/basic-exploration/areas/wormhole",
+                    },
+                    {
+                        label: "无人机区",
+                        link: "/guides/basic-exploration/areas/drone-region",
+                    },
+                ],
+            },
+            {
+                label: "破译",
+                link: "/guides/basic-exploration/hacking",
+            },
+        ],
+    },
 ];
